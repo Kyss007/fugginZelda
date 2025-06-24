8ead663b3188d40fc31210044186c0313cf197a3
keanu <keanucarlo@hotmail.com>
2025-06-24 15:47:50 +0200

    returned dropshadow

---
+++ Assets/scripts/player/moveToGround.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class moveToGround : MonoBehaviour
+{
+    public LayerMask groundLayer;
+
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.parent.position, Vector3.down, out hit, Mathf.Infinity, groundLayer))
+        {
+            transform.position = hit.point;
+        }
+        else
+        {
+            transform.position = transform.parent.position + Vector3.down * 1000000;
+        }
+    }
+}
