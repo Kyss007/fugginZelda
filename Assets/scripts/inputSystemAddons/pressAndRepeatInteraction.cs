b118ac57e1c7e670be089ce069431bdb1e4d70e3
keanu <keanucarlo@hotmail.com>
2025-08-14 12:06:28 +0200

    repeated inputs in menu when hold

---
+++ Assets/scripts/inputSystemAddons/pressAndRepeatInteraction.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class PressAndRepeatInteraction : IInputInteraction
+{
+    public float initialDelay = 0.5f;
+
+    public float repeatInterval = 0.05f;
+    public float pressThreshold = 0.9f;
+
+    private double _nextTriggerTime;
+    private bool _initialFired;
+    private bool _wasActuated;
+
+    static PressAndRepeatInteraction()
+    {
+       InputSystem.RegisterInteraction<PressAndRepeatInteraction>();
+    }
+
+
+    public void Process(ref InputInteractionContext context)
+    {
+        bool isActuated = context.ControlIsActuated(pressThreshold);
+
+        if (context.timerHasExpired)
+        {
+            context.Performed();
+            _nextTriggerTime = context.time + repeatInterval;
+            context.SetTimeout((float)(_nextTriggerTime - context.time));
+            return;
+        }
+
+        if (isActuated && !_wasActuated)
+        {
+            _initialFired = true;
+            context.Performed();
+            _nextTriggerTime = context.time + initialDelay;
+            context.SetTimeout((float)(_nextTriggerTime - context.time));
+        }
+        else if (!isActuated && context.phase != InputActionPhase.Canceled)
+        {
+            _initialFired = false;
+            context.Canceled();
+        }
+
+        _wasActuated = isActuated;
+    }
+
+
+    public void Reset()
+    {
+        _initialFired = false;
+        _nextTriggerTime = 0;
+    }
+}
