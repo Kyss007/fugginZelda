241febb3548ff2781c68c6dfae7100bf02300c3f
keanu <keanucarlo@hotmail.com>
2026-01-24 16:13:49 +0100

    spikeball is stabbable

--- Assets/scripts/enemy/spikeBall.cs
+++ Assets/scripts/enemy/spikeBall.cs
@@ -1,17 +1,34 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class spikeBall : MonoBehaviour
 {
+    public GameObject line;
+
     [Header("Settings")]
     public float velocityThreshold = 5f;
 
     private Rigidbody rb;
     private Animator animator;
 
+    public bool onSword = false;
+
+    private spikeBallTarget spikeBallTarget;
+
+    public SpringJoint springJoint;
+
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
+
+        springJoint = GetComponent<SpringJoint>();
+    }
+
+    private void Start()
+    {
+        spikeBallTarget = FindFirstObjectByType<spikeBallTarget>();
     }
 
     void FixedUpdate()
@@ -19,9 +36,28 @@ public class spikeBall : MonoBehaviour
         float speed = rb.linearVelocity.magnitude;
         //print(speed);
 
-        if (speed >= velocityThreshold)
+        if (speed >= velocityThreshold && !onSword)
         {
             animator.SetTrigger("spikeUp");
+        }
+    }
+
+    public void reciveHit(Vector3 dir, string attackName)
+    {
+        if(onSword)
+            return;
+
+        if(attackName == "frontStab")
+        {
+            line.SetActive(false);
+
+            Destroy(springJoint);
+            rb.detectCollisions = false;
+            rb.isKinematic = true;
+            transform.parent = spikeBallTarget.transform;
+            transform.localPosition = Vector3.zero;
+
+            onSword = true;
         }
     }
 }
