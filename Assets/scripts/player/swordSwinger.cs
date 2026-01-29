a2bc5c7d257a67b04c47abefe641a42cf7f3173c
keanu <keanucarlo@hotmail.com>
2026-01-29 14:32:40 +0100

    added buffer time to sword stab after dodge

--- Assets/scripts/player/swordSwinger.cs
+++ Assets/scripts/player/swordSwinger.cs
@@ -24,6 +24,10 @@ public class swordSwinger : MonoBehaviour
 
     public bool disableSword = false;
 
+    private float dodgeDisableTimer = 0f;
+    public float dodgeBufferTime = 0.2f; 
+
+
     private void Start()
     {
         damageProvider = GetComponentInParent<damageProvider>();
@@ -45,26 +49,11 @@ public class swordSwinger : MonoBehaviour
         if (inputProvider == null)
             return;
 
+        // --- Attack Logic ---
         if (inputProvider.getAttackInput() && !hasTriggeredAttackThisButtonPress)
         {
-            if (targetController.isTargeting)
-            {
-                if (movementDriver.isJumping)
-                {
-                    animator.SetBool("jumpSwing", true);
-                }
-                else
-                {
-                    animator.SetBool("jumpSwing", false);
-                }
-            }
-            else
-            {
-                animator.SetBool("jumpSwing", false);
-            }
-
+            // ... (existing attack logic)
             animator.SetTrigger("swing");
-
             hasTriggeredAttackThisButtonPress = true;
         }
         else if (!inputProvider.getAttackInput() && hasTriggeredAttackThisButtonPress)
@@ -72,13 +61,32 @@ public class swordSwinger : MonoBehaviour
             hasTriggeredAttackThisButtonPress = false;
         }
 
-        animator.SetBool("isDodge", movementDriver.isDodging);
+        // --- Improved Dodge Logic with Delay ---
+        if (movementDriver.isDodging)
+        {
+            // Set true instantly and reset the timer
+            animator.SetBool("isDodge", true);
+            dodgeDisableTimer = dodgeBufferTime; 
+        }
+        else
+        {
+            // If we aren't dodging, countdown the timer
+            if (dodgeDisableTimer > 0)
+            {
+                dodgeDisableTimer -= Time.deltaTime;
+            }
+            else
+            {
+                // Only set to false once the timer hits zero
+                animator.SetBool("isDodge", false);
+            }
+        }
 
+        // --- Grounded Logic ---
         if (movementDriver.isGrounded && !wasGrounded)
         {
             animator.SetBool("jumpSwing", false);
         }
-
         wasGrounded = movementDriver.isGrounded;
     }
 
